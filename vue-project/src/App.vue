9cb124d6ebaa8361be82b7d0c61f4d2deff1b0b1
anjali1102 <ac.chauhan1102@gmail.com>
2023-01-14 19:32:09 +0530

    vueJs via command line

---
+++ vue-project/src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+// import { RouterLink, RouterView } from "vue-router";
+// import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <header>
+    <h3>hello VueJS</h3>
+    <h1>I am Anjali 🌻</h1>
+  </header>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+header .wrapper {
+  display: flex;
+  place-items: flex-start;
+  flex-wrap: wrap;
+}
+
+nav {
+  text-align: left;
+  margin-left: -1rem;
+  font-size: 1rem;
+
+  padding: 1rem 0;
+  margin-top: 1rem;
+}
+</style>
